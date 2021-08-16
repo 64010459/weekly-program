16ba5a15427d50b9ef68f03ca1a70b7d1e6c741a
64010459 <64010459@kmitl.ac.th>
2021-08-16 18:28:46 +0700

    Create Source.cpp
    บุณยวีร์ กรีแสง

---
+++ Source.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    float a, b;
+    printf("Enter two numbers you want to operate\n");
+    scanf("%f %f", &a, &b);
+    printf("Sum = ");
+    printf("%.2f", a + b);
+    printf("\nDifference = ");
+    printf("%.2f", a - b);
+    printf("\nProduct = ");
+    printf("%.2f", a * b);
+    printf("\nQuotient = ");
+    printf("%.2f", a / b);
+    return 0;
+}
